680a57cafa3bd4fc0f1b95d4df320222413b1ab5
Agustin <agustin.ledezma@ucb.edu.bo>
2024-05-26 17:26:06 -0400

    Se implemento el archivo GreetingDelivery y se creo la clase GreetingDelivery, con sus atributos

---
+++ src/birthday/GreetingDelivery.js
@@ -0,0 +1,8 @@
+export class GreetingDelivery {
+    constructor(transport, smtpUrl, smtpPort) {
+        this.transport = transport;
+        this.smtpUrl = smtpUrl;
+        this.smtpPort = smtpPort;
+    }
+
+}
